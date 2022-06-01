8d3bb340c111087c1f055e68c2e1ce2a4fb07cee
Toni <toninataliet@gmail.com>
2022-06-01 10:47:29 -0700

    modularised save_csv - tested

--- Module_2_Challenge/Starter_Code/loan_qualifier_app/qualifier/utils/fileio.py
+++ Module_2_Challenge/Starter_Code/loan_qualifier_app/qualifier/utils/fileio.py
@@ -28,3 +28,28 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+
+def save_csv(qualifying_loans):
+    """Qualifying loans from find_qualifying_loans function.
+
+    Args:
+        save_path: The csv destination file path. Currently static - replace with variable!
+
+    Returns:
+        A CSV file with qualifying loans in rows."""
+
+    save_path = "testfile.csv"
+    save_path = Path(save_path)
+    
+    header = "Lender","Max Loan Amount","Max LTV","Max DTI","Min Credit Score","Interest Rate"
+
+    with open(save_path, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        #write header row to CSV file
+        csvwriter.writerow(header)
+    
+        #add each loan in qualifying loans list to CSV
+        for loan in qualifying_loans:
+            csvwriter.writerow(loan)
